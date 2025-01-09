90c47726977ffbab1c8cab41509762da63e9a430
Nguyen Tien Thanh <thanhnthe163528@fpt.edu.vn>
2025-01-09 23:34:35 +0700

    feat: completed Navbar component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
+import Hero from "@/sections/Hero";
 import Navbar from "@/sections/Navbar";
 
 export default function Home() {
   return (
     <>
       <Navbar />
+      <Hero />
     </>
   );
 }
--- src/sections/Navbar.tsx
+++ src/sections/Navbar.tsx
@@ -12,18 +12,36 @@ const navLinks = [
 
 export default function Navbar() {
   return (
-    <section className="py-4">
+    <section className="py-4 lg:py-8">
       <div className="container">
-        <div className="grid grid-cols-2 border border-white/15 rounded-full py-2 px-4 items-center">
+        <div className="grid grid-cols-2 lg:grid-cols-3 border border-white/15 rounded-full py-2 px-4 items-center">
           <div>
-            <Image src={logoImage} alt="Layers Logo" className="h-9 w-auto" />
+            <Image
+              src={logoImage}
+              alt="Layers Logo"
+              className="h-9 w-auto md:h-auto"
+            />
           </div>
-          <div className="flex justify-end">
+          <div className="lg:flex justify-center items-center hidden">
+            <nav className="flex gap-6 font-medium">
+              {navLinks.map((link) => (
+                <a href={link.href} key={link.label}>
+                  {link.label}
+                </a>
+              ))}
+            </nav>
+          </div>
+          <div className="flex justify-end gap-2">
             <Menu className="md:hidden" />
-            <Button className="border border-white/80 rounded-full bg-transparent mr-2">
+            <Button className="border border-white/80 rounded-full bg-transparent md:inline-flex hidden">
               Log In
             </Button>
-            <Button>Sign Up</Button>
+            <Button
+              className="bg-yellow-400 text-primary rounded-full md:inline-flex hidden"
+              variant={"outline"}
+            >
+              Sign Up
+            </Button>
           </div>
         </div>
       </div>
