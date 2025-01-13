78917aa05b4a150741a5768e0a21bf515c93f3df
Nguyen Tien Thanh <thanhnthe163528@fpt.edu.vn>
2025-01-13 13:15:23 +0700

    feat: add CallToAction section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+import CallToAction from "@/sections/CallToAction";
 import Faqs from "@/sections/Faqs";
 import Features from "@/sections/Features";
 import Hero from "@/sections/Hero";
@@ -16,6 +17,7 @@ export default function Home() {
       <Features />
       <Integrations />
       <Faqs />
+      <CallToAction />
     </>
   );
 }
--- src/sections/CallToAction.tsx
+++ src/sections/CallToAction.tsx
@@ -1,3 +1,18 @@
 export default function CallToAction() {
-    return <div>Call To Action</div>;
+  return (
+    <section className="py-24">
+      <div className="overflow-x-clip p-4 flex">
+        <div className="flex flex-none gap-16 text-6xl md:text-7xl font-medium">
+          {Array.from({ length: 10 }).map((_, index) => (
+            <div className="flex items-center gap-6" key={index}>
+              <span className="text-blue-400">&#10038;</span>
+              <span>
+                Try <span className="text-lime-400">Layers</span> for free
+              </span>
+            </div>
+          ))}
+        </div>
+      </div>
+    </section>
+  );
 }
